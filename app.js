77b5b267a7f586f3c4d55e8e11edf8916ba78a35
martinsewere <martinsewere@gmail.com>
2018-04-24 12:39:47 -0700

    adding javascript codes with some modifications  to hmtl

--- app.js
+++ app.js
@@ -8,3 +8,114 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+document.querySelector(".dice").style.display = "none"; 
+
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+
+document.querySelector(".btn-roll").addEventListener("click", function() {
+
+	// 1. We need a Random Number after Click
+	var dice = Math.floor(Math.random() * 6) + 1;
+
+	// 2. Display the Random Dice number
+	var diceDOM = document.querySelector(".dice");
+	diceDOM.style.display = "block";
+	diceDOM.src = "dice-" + dice + ".png";
+
+
+
+	// 3. Update the round score IF the rolled number was NOT a 1
+	if (dice !== 1) {
+		//Add score
+		roundScore += dice; // same as |roundScore = roundScore + dice;|
+		document.querySelector("#current-" + activePlayer).textContent = roundScore;
+	} else {
+		//Next Player
+		nextPlayer();
+		//Next Player
+		// activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		// same as below------------
+		// if(activePlayer === 0) {
+		// 	activePlayer = 1;
+		// } else {
+		// 	activePlayer = 0;
+		// }------------------------
+		// roundScore = 0;
+
+		// document.getElementById("current-0").textContent = "0";
+		// document.getElementById("current-1").textContent = "0";
+
+		// document.querySelector(".player-0-panel").classList.toggle("active");
+		// document.querySelector(".player-1-panel").classList.toggle("active");
+        // Used to add or remove a class styling, toggle is also used
+		// document.querySelector(".player-0-panel").classList.remove("active");
+		// document.querySelector(".player-1-panel").classList.add("active");
+
+        // document.querySelector(".dice").style.display = "none";
+
+	}
+
+});
+
+document.querySelector(".btn-hold").addEventListener("click", function() {
+	// Add CURRENT score to GLOBAL score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+	// Check if the player won the game
+
+
+	//Next Player
+	nextPlayer();
+
+});
+
+
+function nextPlayer() {
+	// Next Player
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	// same as below------------
+	// if(activePlayer === 0) {
+	// 	activePlayer = 1;
+	// } else {
+	// 	activePlayer = 0;
+	// }------------------------
+	roundScore = 0;
+
+	document.getElementById("current-0").textContent = "0";
+	document.getElementById("current-1").textContent = "0";
+
+	document.querySelector(".player-0-panel").classList.toggle("active");
+	document.querySelector(".player-1-panel").classList.toggle("active");
+    // Used to add or remove a class styling, toggle is also used
+	// document.querySelector(".player-0-panel").classList.remove("active");
+	// document.querySelector(".player-1-panel").classList.add("active");
+
+    document.querySelector(".dice").style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+// document.querySelector("#current-" + activePlayer).textContent = dice;
+//document.querySelector("#current-" + activePlayer).innerHTML = "<em>" + dice + "</em>";
+// var x = document.querySelector("#score-0").textContent;
